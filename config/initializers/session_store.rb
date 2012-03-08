7b8c4cfc1c75e8e20d3a7a44759d22d357b5b0cf
Noah Heldman <noah.heldman@gmail.com>
2012-03-07 21:42:20 -0800

    Initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CodeScreen::Application.config.session_store :cookie_store, key: '_code_screen_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CodeScreen::Application.config.session_store :active_record_store
